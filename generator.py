aeb0bf553ff18a0510560cd7a64732df100ef3fc
Arpita Pani <115695971+Jexa07@users.noreply.github.com>
2025-07-14 16:13:39 +0530

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,7 +1,7 @@
 from openai import OpenAI
 import os
 
-client = OpenAI(api_key=os.getenv("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"))
+client = OpenAI(api_key=os.getenv("YOUR API KEY"))
 
 def generate_answer_openai(prompt, model="gpt-3.5-turbo"):
     try:
